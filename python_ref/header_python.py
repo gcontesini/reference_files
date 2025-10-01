82bda544d410ab388e06ca636833120207ad87da
contesini <gcontesini@gmail.com>
2025-09-30 21:20:00 -0400

    chore: organize python reference files
     - update python doc string with new __version__ functionality

--- python_ref/header_python.py
+++ python_ref/header_python.py
@@ -1,28 +1,24 @@
 
-
+# ==============================================================================
+import git as git
+version = len( list( git.Repo(".").iter_commits( 'HEAD' ) ) )
+released = 0
+patched = 0
+# ==============================================================================
 import datetime as dt
-
 __author__    = f"Guilherme Contesini dos Santos"
 __copyright__ = f"Copyright (c) 2025 {__author__}"
 __license__   = f"GLPv3"
-__version__   = f"0.0.1"
+__version__   = f"{released}.{patched}.{version}"
 __email__     = f"gcontesini@gmail.com"
 __status__    = f"under development"
 __date__      = f"{dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S_')}"
-
-"""
-Short description of this is
-
-More detailed explanation, be creative, no one will ever read this anyways.
-"""
-
 # ==============================================================================
 
 def main():
     pass
-
-
 # ==============================================================================
-# unit test
+# >> unit test
 if __name__ == "__main__":
     main()
+# ==============================================================================
